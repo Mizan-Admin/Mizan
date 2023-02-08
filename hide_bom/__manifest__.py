535d3f4a7d65737af6443597c96810ff7002cbda
Mizan-Admin <108893215+Mizan-Admin@users.noreply.github.com>
2023-02-08 18:04:33 +0200

    Hide Bom

---
+++ hide_bom/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide BOM",
+    'version': '15.0.1.0.0',
+    'license': 'OPL-1',
+    'depends': [
+         'mrp',
+    ],
+    'data':[ 
+        'security/group_bom.xml',
+        'views/bom_product_view.xml',
+    ],
+    'application': True,
+}
