2eefec5e34465a51a68de585bd9ec11fa9ffc96d
Mizan-Admin <108893215+Mizan-Admin@users.noreply.github.com>
2023-02-08 18:05:14 +0200

    Hide cost from system

---
+++ hide_cost/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide Product Cost",
+    'version': '15.0.1.0.0',
+    'license': 'OPL-1',
+    'depends': [
+         'sale_management',
+    ],
+    'data':[ 
+        'security/group_cost.xml',
+        'views/product_view.xml'
+    ],
+    'application': True,
+}
